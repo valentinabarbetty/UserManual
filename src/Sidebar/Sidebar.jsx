f66d8362b36e6cd5947b0218307a85b0d53f421a
valentinabarbetty <valentinabarbetty2@gmail.com>
2024-11-11 18:31:14 -0500

    front

--- src/Sidebar/Sidebar.jsx
+++ src/Sidebar/Sidebar.jsx
@@ -1,9 +1,270 @@
-import React from 'react'
-
+import React from 'react';
+import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import { Menu } from 'antd';
+const items = [
+  {
+    key: 'sub1',
+    label: 'Conoce los conceptos básicos de Spotify',
+    icon: <MailOutlined />,
+    children: [
+      {
+        key: 'g1',
+        label: 'Item 1',
+        type: 'group',
+        children: [
+          {
+            key: '1',
+            label: 'Option 1',
+          },
+          {
+            key: '2',
+            label: 'Option 2',
+          },
+        ],
+      },
+      {
+        key: 'g2',
+        label: 'Item 2',
+        type: 'group',
+        children: [
+          {
+            key: '3',
+            label: 'Option 3',
+          },
+          {
+            key: '4',
+            label: 'Option 4',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    key: 'sub2',
+    label: 'Crea tu cuenta',
+    icon: <AppstoreOutlined />,
+    children: [
+      {
+        key: '5',
+        label: 'Option 5',
+      },
+      {
+        key: '6',
+        label: 'Option 6',
+      },
+      {
+        key: 'sub3',
+        label: 'Submenu',
+        children: [
+          {
+            key: '7',
+            label: 'Option 7',
+          },
+          {
+            key: '8',
+            label: 'Option 8',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    type: 'divider',
+  },
+  {
+    key: 'sub4',
+    label: 'Explora la biblioteca de Spotify',
+    icon: <SettingOutlined />,
+    children: [
+      {
+        key: '9',
+        label: 'Option 9',
+      },
+      {
+        key: '10',
+        label: 'Option 10',
+      },
+      {
+        key: '11',
+        label: 'Option 11',
+      },
+      {
+        key: '12',
+        label: 'Option 12',
+      },
+    ],
+  },  {
+    key: 'sub5',
+    label: 'Crea listas de reproducción',
+    icon: <SettingOutlined />,
+    children: [
+      {
+        key: '9',
+        label: 'Option 9',
+      },
+      {
+        key: '10',
+        label: 'Option 10',
+      },
+      {
+        key: '11',
+        label: 'Option 11',
+      },
+      {
+        key: '12',
+        label: 'Option 12',
+      },
+    ],
+  },
+  {
+    key: 'sub6',
+    label: 'Descubre y sigue nuevos artistas',
+    icon: <SettingOutlined />,
+    children: [
+      {
+        key: '9',
+        label: 'Option 9',
+      },
+      {
+        key: '10',
+        label: 'Option 10',
+      },
+      {
+        key: '11',
+        label: 'Option 11',
+      },
+      {
+        key: '12',
+        label: 'Option 12',
+      },
+    ],
+  },
+  {
+    key: 'sub7',
+    label: 'Escucha sin conexión',
+    icon: <SettingOutlined />,
+    children: [
+      {
+        key: '9',
+        label: 'Option 9',
+      },
+      {
+        key: '10',
+        label: 'Option 10',
+      },
+      {
+        key: '11',
+        label: 'Option 11',
+      },
+      {
+        key: '12',
+        label: 'Option 12',
+      },
+    ],
+  },
+  {
+    key: 'sub8',
+    label: 'Personaliza la calidad de sonido',
+    icon: <SettingOutlined />,
+    children: [
+      {
+        key: '9',
+        label: 'Option 9',
+      },
+      {
+        key: '10',
+        label: 'Option 10',
+      },
+      {
+        key: '11',
+        label: 'Option 11',
+      },
+      {
+        key: '12',
+        label: 'Option 12',
+      },
+    ],
+  },
+  {
+    key: 'sub9',
+    label: 'Comparte tu música',
+    icon: <SettingOutlined />,
+    children: [
+      {
+        key: '9',
+        label: 'Option 9',
+      },
+      {
+        key: '10',
+        label: 'Option 10',
+      },
+      {
+        key: '11',
+        label: 'Option 11',
+      },
+      {
+        key: '12',
+        label: 'Option 12',
+      },
+    ],
+  },
+  {
+    key: 'sub4',
+    label: 'Explora Spotify Premium',
+    icon: <SettingOutlined />,
+    children: [
+      {
+        key: '9',
+        label: 'Option 9',
+      },
+      {
+        key: '10',
+        label: 'Option 10',
+      },
+      {
+        key: '11',
+        label: 'Option 11',
+      },
+      {
+        key: '12',
+        label: 'Option 12',
+      },
+    ],
+  },
+  {
+    key: 'grp',
+    label: 'Group',
+    type: 'group',
+    children: [
+      {
+        key: '13',
+        label: 'Option 13',
+      },
+      {
+        key: '14',
+        label: 'Option 14',
+      },
+    ],
+  },
+];
 const Sidebar = () => {
+  const onClick = (e) => {
+    console.log('click ', e);
+  };
   return (
-    <div>Sidebar</div>
-  )
-}
-
-export default Sidebar
+    <Menu
+      onClick={onClick}
+      style={{
+        width: 256,
+        backgroundColor: '#2a2a2a', // Color de fondo
+        color: '#fffff', // Color de las letras
+      }}
+      defaultSelectedKeys={['1']}
+      defaultOpenKeys={['sub1']}
+      mode="inline"
+      items={items}
+      theme="dark"
+    />
+  );
+};
+export default Sidebar;
