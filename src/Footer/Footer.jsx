906c030970aafa1964d28f5dcf7071593b4e3b09
valentinabarbetty <valentinabarbetty2@gmail.com>
2024-11-15 09:58:49 -0500

    act

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,20 +1,27 @@
-import React from 'react';
-import { Row, Col, Typography } from 'antd';
-import './Footer.css';  // Asegúrate de importar el archivo de estilos
+import React from "react";
+import { Row, Col, Typography } from "antd";
+import "./Footer.css"; // Asegúrate de importar el archivo de estilos
 
 const { Text } = Typography;
 
 const Footer = () => {
   return (
     <div className="footer">
-  <Row justify="center" align="middle" style={{ width: '100%' }}>
-    {/* Columna de texto */}
-    <Col xs={24} sm={12} md={8} lg={6} className="footer-column">
-      <Text className="footer-text">© 2024 Spotify BV</Text>
-    </Col>
-  </Row>
-</div>
-
+      <Row justify="center" align="middle" style={{ width: "100%" }}>
+        <Col xs={24} sm={12} md={8} lg={6} className="footer-column">
+          <Text
+            style={{
+              color: "white",
+              fontWeight: "bold",
+              fontFamily: "var(--font-family)",
+              fontSize: "var(--font-size-body)",
+            }}
+          >
+            © 2024 Spotify BV
+          </Text>
+        </Col>
+      </Row>
+    </div>
   );
 };
 
